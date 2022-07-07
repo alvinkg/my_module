8c0e615ea60eb59cf467eeccf7a40c45e9de977d
alvinkg <lusnalvin@yahoo.com>
2022-07-07 14:50:27 +0800

    9.11 Defining document-style forms Fixes #13

--- __manifest__.py
+++ __manifest__.py
@@ -2,12 +2,12 @@
 
 {
     'name': 'My Module', # Module Title
-    'description':"""Long Description""",
+    'description': """Long Description""",
     'author': 'Alvin Lim', 
     'website': "www.konvergenttech.com",
     'category': "Uncategorized",
     'version': "14.0.1",
-    'depends': ['base', ], #'account'
+    'depends': ['base', 'account', 'mail'],
     'data': [
         'views/my_contacts.xml',
         # 'views/my_contacts_fr.xml',
